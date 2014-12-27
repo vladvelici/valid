630fd953a7012959f87504176df2d93254c57b90
Vlad Velici <vlad.velici@gmail.com>
2014-12-27 17:19:21 +0000

    Fixed a few documentation mistakes.

--- string.go
+++ string.go
@@ -6,11 +6,17 @@ import (
 	"regexp"
 )
 
+// StringValidator interface is implemented by all string validators.
 type StringValidator interface {
+	// Validate validates the given parameter and returns a validation error, or nil
+	// if the input is valid.
 	Validate(string) error
 }
 
-// StringValidator takes a value to validate and returns a validation error.
+// A StringFucn takes a value to validate and returns a validation error.
+//
+// This type implements the StringValidator interface, thus any functions with this
+// signature can be casted to StringFunc and used as a StringValidator.
 type StringFunc func(val string) error
 
 // Validate function of StringFunc
@@ -37,7 +43,7 @@ func Regexp(pattern, message string) StringFunc {
 	return RegexpCompiled(reg, message)
 }
 
-// Regexp creates a regular expression validator, using the given already-compiled *regexp.Regexp.
+// RegexpCompiled creates a regular expression validator, using the given already-compiled *regexp.Regexp.
 func RegexpCompiled(reg *regexp.Regexp, message string) StringFunc {
 	f := func(val string) error {
 		if reg.MatchString(val) {
@@ -84,7 +90,7 @@ func MaxLen(max int, message string) StringValidator {
 	return StringFunc(f)
 }
 
-// Len creates a length validator, that checks if the length of given string is in the open interval (min, max).
+// LenStrict creates a length validator, that checks if the length of given string is in the open interval (min, max).
 // It does not include min and max: all val that satisfy max > len(val) > min are considered valid.
 func LenStrict(min, max int, message string) StringFunc {
 	f := func(val string) error {
@@ -108,7 +114,7 @@ func MinLenStrict(min int, message string) StringValidator {
 	return StringFunc(f)
 }
 
-// MaxLen creates a maximum length string validator that consideres all strings val valid if they
+// MaxLenStrict creates a maximum length string validator that consideres all strings val valid if they
 // satisfy len(val) < max.
 func MaxLenStrict(max int, message string) StringValidator {
 	f := func(val string) error {
@@ -133,13 +139,13 @@ func Nonempty(message string) StringValidator {
 
 // Common regular expressions used for validation.
 var (
-	// Alphanumeric characters
+	// RegAlphanumeric matches alphanumeric characters
 	RegAlphanumeric = regexp.MustCompile("^[a-zA-Z0-9]*$")
 
-	// Alphanumeric characters and -, _ and .
+	// RegAlphanumericPermissive matches numbers, letters,  -, _, and .
 	RegAlphanumericPermissive = regexp.MustCompile("^[a-zA-Z0-9-_.]*$")
 
-	// E-mail addresses, according to W3C http://www.w3.org/TR/html-markup/input.email.html#input.email.attrs.value.single
+	// RegEmail is the HTML5 E-mail address regular expression, according to W3C http://www.w3.org/TR/html-markup/input.email.html#input.email.attrs.value.single
 	RegEmail = regexp.MustCompile("^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$")
 )
 
